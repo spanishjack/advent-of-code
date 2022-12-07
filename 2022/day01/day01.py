1dfa347fc416322f8a4d7ef5e29440ab6dc31f1d
spanishjack <jhuck03@gmail.com>
2022-12-07 14:52:02 -0800

    Update day01.py

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -1,10 +1,13 @@
-with open('./2022/day01/input.txt') as f:
+input_file = '/Users/jhuck/Documents/GitHub/advent-of-code/2022/day01/input.txt'
+
+with open(input_file) as f:
     s = f.readlines()
 
 s = [x.strip() for x in s]
 
 k  = 0
 r = 0
+t = []
 
 for i in s:
     if i.isnumeric():
@@ -12,5 +15,10 @@ for i in s:
     else:
         if k  > r:
             r = int(k)
+        t.append(k)
         k  = 0
+
+#print top value
 r
+#print sum top 3 values
+sum(sorted(t, reverse=True)[:3])
