d3122f3fe2d5191bd3dd98cf40ec235dbb31542d
spanishjack <jhuck03@gmail.com>
2022-12-07 14:51:42 -0800

    Update day01.py

--- 2022/day01/day01.py
+++ 2022/day01/day01.py
@@ -2,18 +2,15 @@ with open('./2022/day01/input.txt') as f:
     s = f.readlines()
 
 s = [x.strip() for x in s]
-print(s)
 
-k = 0
+k  = 0
 r = 0
 
 for i in s:
     if i.isnumeric():
-        k = k + int(i)
+        k  = k  + int(i)
     else:
-        if k > r:
+        if k  > r:
             r = int(k)
-        k = 0
+        k  = 0
 r
-
-
