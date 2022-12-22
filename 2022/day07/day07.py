5870a51aa3f0b1176fceb30a1be4cc5bd409f987
spanishjack <jhuck03@gmail.com>
2022-12-21 16:09:58 -0800

    day 7

--- 2022/day07/day07.py
+++ 2022/day07/day07.py
@@ -1,16 +1,15 @@
 input_file = '/Users/JHuck/Documents/GitHub/advent-of-code/2022/day07/input.txt'
 
-import pandas as pd
-
 with open(input_file) as f:
     s = f.readlines()
 
 s = [x.strip().split(' ') for x in s]
 
 class Node(object):
-    def __init__(self, data, type, size=0):
+    def __init__(self, data, type, parent, size=0):
         self.data = data
         self.type = type
+        self.parent = parent
         self.size = size
         self.children = []
 
@@ -31,15 +30,12 @@ cd = []
 nl = []
 
 
-for i in nl:
-    i.data
-
 for i in s:
     if i[1] == 'cd':
         if i[2] == '..':
             cd.pop()
         elif i[2] == '/':
-            nl.append(Node(i[2],'dir'))
+            nl.append(Node(i[2],'dir',''))
             cd.append(i[2])
         else:
             cd.append(i[2])
@@ -47,24 +43,35 @@ for i in s:
         for x in nl:
             if x.data == cd[-1]:
                 x.add_child(i[1])
-        nl.append(Node(i[1],'dir'))
+        nl.append(Node(i[1],'dir',cd[-1]))
     if i[0].isdigit():
         for x in nl:
             if x.data == cd[-1]:
                 x.add_child(i[1])
-        nl.append(Node(i[1],'file',i[0])) 
+        nl.append(Node(i[1],'file',cd[-1],i[0])) 
 
 size = []
-#dir_list = []
+cl = []
 
 for i in nl:
-    size.append(i.size)
-    dir_list = 
-    for x in dir_list:
-        size.append(i.size)
-        if x.type == 'dir':
-            dir_list.append(x.children)
+    if int(i.size) > 0:
+        size.append([i.parent,i.size])
+    cl.append(i.children)
+    for x in cl:
+        for x1 in nl:
+            x1.data
+            x
+            if x == x1.data:
+                if int(x1.size) > 0:
+                    size.append([i.parent,i.size])
+                cl.append(x1.children)
+                x
+                x1
+        cl.remove(x)
 
+
+for i in tt:
+    if '4' in
 for i in dir_list:
     i.data
     for n in i.children:
